efada92c5a9a051d3673059bdedcddd669c9224d
Sajal Sangal <sajalsangal@gmail.com>
2025-10-02 00:00:54 +0530

    Added a checkbox for dark theme

--- streamlit_dashboard.py
+++ streamlit_dashboard.py
@@ -13,7 +13,44 @@ warnings.filterwarnings("ignore", category= UserWarning, module= "matplotlib")
 sns.set_theme(style= "whitegrid") #Set theme for graphs
 
 #Navigation Bar Properties
-st.sidebar.title("Page Options")
+sidebarCol1, sidebarCol2 = st.sidebar.columns([.7,.3], vertical_alignment="center") # 70% and 30% width
+with sidebarCol1:
+    st.title("Page Options")
+
+with sidebarCol2:
+    theme = st.checkbox(label=":material/nightlife:", key= "theme")
+    if theme:
+        st.markdown("""
+    <style>
+    /* Main background */
+    .stApp {
+        background-color: #121212;
+        color: #E0E0E0;
+    }
+
+    /* Sidebar background */
+    section[data-testid="stSidebar"] {
+        background-color: #1E1E1E;
+        color: #E0E0E0;
+    }
+
+    /* Sidebar text */
+    section[data-testid="stSidebar"] * {
+        color: #E0E0E0 !important;
+    }
+
+    /* Headings */
+    h1, h2, h3, h4, h5, h6 {
+        color: #FFFFFF !important;
+    }
+
+    /* Streamlit widgets (labels, radio, selectbox etc.) */
+    label, .stText, .stSelectbox, .stRadio, .stCheckbox {
+        color: #E0E0E0 !important;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
 page = st.sidebar.selectbox(
     label= "Select Page", 
     options= ["Home Page", "Univariate Analysis", "Bi-Variate Analysis", "MultiVariate Analysis"], 
